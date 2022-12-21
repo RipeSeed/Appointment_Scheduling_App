56f2310694601834a9e592da7d494efcf3c7b656
sidra rasool <sidra.rasool30@gmail.com>
2022-12-21 14:49:57 +0500

    bugs resolved and timezones handled

--- index.ts
+++ index.ts
@@ -1,10 +1,20 @@
 import express from "express";
+const dayjs = require("dayjs");
 import { initializeApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { Config } from "./types";
 initializeApp({
   credential: cert("./cert.json"),
 });
+var isSameOrAfter = require("dayjs/plugin/isSameOrAfter");
+dayjs.extend(isSameOrAfter);
+var isSameOrBefore = require("dayjs/plugin/isSameOrBefore");
+dayjs.extend(isSameOrBefore);
+var utc = require("dayjs/plugin/utc");
+var timezone = require("dayjs/plugin/timezone"); // dependent on utc plugin
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
 const db = getFirestore();
 const app = express();
 const port = 3000;
@@ -49,15 +59,33 @@ app.post("/create-event", async (req, res) => {
   }
   const startTime = req.body.startTime;
   const duration = Number(req.body.duration);
+
+  const currentTime = dayjs.utc();
+  const newTime = dayjs(startTime).utc();
+  const doctorStartTime = dayjs
+    .tz(dayjs(startTime).utc().format("YYYY-MM-DD"), "America/Los_Angeles")
+    .hour(config.startHours);
+  const doctorEndTime = dayjs
+    .tz(dayjs(startTime).utc().format("YYYY-MM-DD"), "America/Los_Angeles")
+    .hour(config.endHours);
+
+  if (
+    newTime.isBefore(currentTime) ||
+    newTime.isBefore(doctorStartTime) ||
+    newTime.isAfter(doctorEndTime)
+  ) {
+    return res.send("Invalid Time");
+  }
   try {
     const querySnapshot = await db.collection("events").get();
     const events = querySnapshot.docs.map((doc) => doc.data());
+
     const newEvent = createEvent(startTime, events, duration);
     if (newEvent) {
       const response = await db.collection("events").doc().set(newEvent);
       res.send(response);
     } else {
-      res.send("Event already exists");
+      res.status(422).send("Event already exists");
     }
   } catch (error) {
     console.log("Error getting events from firestore:", error);
@@ -84,37 +112,76 @@ app.post("/all-events", async (req, res) => {
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
-// Function to calculate the available slots based on the events and the static config
+
 function getAvailableSlots(
   events: FirebaseFirestore.DocumentData[],
   config: Config,
   userDate: string,
   timeZone: string
 ) {
+  const dayBefore = dayjs(userDate).subtract(1, "day").format("YYYY-MM-DD");
+  const dayAfter = dayjs(userDate).add(1, "day").format("YYYY-MM-DD");
+  const allSlots: string[] = [];
+  const dayBeforeEvents: FirebaseFirestore.DocumentData[] = filterEventsbyDate(
+    events,
+    dayBefore
+  );
+  allSlots.push(
+    ...getDailySlots(dayBeforeEvents, config, dayBefore, timeZone, userDate)
+  );
+  const userDateEvents: FirebaseFirestore.DocumentData[] = filterEventsbyDate(
+    events,
+    userDate
+  );
+  allSlots.push(
+    ...getDailySlots(userDateEvents, config, userDate, timeZone, userDate)
+  );
+  const dayAfterEvents: FirebaseFirestore.DocumentData[] = filterEventsbyDate(
+    events,
+    dayAfter
+  );
+  allSlots.push(
+    ...getDailySlots(dayAfterEvents, config, dayAfter, timeZone, userDate)
+  );
+  return allSlots;
+}
+
+// Function to calculate the available slots based on the events and the static config
+function getDailySlots(
+  events: FirebaseFirestore.DocumentData[],
+  config: Config,
+  currentDate: string,
+  timeZone: string,
+  userDate: string
+) {
   // Create a list of available slots
-  const availableSlots: Date[] = [];
-  // Get the start and end times based on the static config
-  const startTime = new Date(userDate);
-  startTime.setHours(config.startHours, 0, 0, 0);
-  const endTime = new Date(userDate);
-  endTime.setHours(config.endHours, 0, 0, 0);
-  // Set the current time to the start time
+  const availableSlots: string[] = [];
+  const startTime = dayjs
+    .tz(currentDate, "America/Los_Angeles")
+    .hour(config.startHours);
+  const endTime = dayjs
+    .tz(currentDate, "America/Los_Angeles")
+    .hour(config.endHours);
   let currentTime = startTime;
   // Loop through the events and add the available slots to the list
   while (currentTime <= endTime) {
     // Check if there is an event at the current time
     const event = events.find(
       (event: FirebaseFirestore.DocumentData) =>
-        event.startTime.toDate().getTime() === currentTime.getTime() ||
-        event.endTime.toDate().getTime() > currentTime.getTime()
+        dayjs(event.startTime).tz(config.timezone).isSame(currentTime) ||
+        dayjs(event.endTime).tz(config.timezone).isAfter(currentTime)
     );
     // If there is no event at the current time, add it to the list of available slots
-    if (!event) {
-      // availableSlots.push(currentTime.toISOString());
-      availableSlots.push(convertTZ(currentTime, timeZone));
+    if (
+      !event &&
+      dayjs.utc(currentTime).tz(timeZone).format("YYYY-MM-DD") === userDate
+    ) {
+      availableSlots.push(
+        dayjs.utc(currentTime).tz(timeZone).format("YYYY-MM-DDTHH:mm:ssZ")
+      );
     }
     // Increment the current time by the duration
-    currentTime = new Date(currentTime.getTime() + config.duration * 60 * 1000);
+    currentTime = currentTime.add(config.duration, "m");
   }
   return availableSlots;
 }
@@ -126,13 +193,18 @@ function createEvent(
 ) {
   const event = events.find(
     (event: FirebaseFirestore.DocumentData) =>
-      event.startTime.toDate().getTime() === new Date(startTime).getTime()
+      dayjs(event.startTime).isSame(startTime) ||
+      dayjs(startTime).isBefore(dayjs(event.endTime))
   );
   let newEvent;
   if (!event) {
     newEvent = {
-      startTime: new Date(startTime),
-      endTime: new Date(new Date(startTime).getTime() + duration * 60 * 1000),
+      date: dayjs(startTime).utc().format("YYYY-MM-DD"),
+      startTime: dayjs(startTime).utc().format("YYYY-MM-DDTHH:mm:ssZ"),
+      endTime: dayjs(startTime)
+        .add(duration, "m")
+        .utc()
+        .format("YYYY-MM-DDTHH:mm:ssZ"),
       duration: duration,
     };
   }
@@ -146,17 +218,19 @@ function getFilteredEvents(
 ) {
   const filteredEvents = events.filter(
     (event: FirebaseFirestore.DocumentData) =>
-      event.startTime.toDate().getTime() >= startDate.getTime() &&
-      event.endTime.toDate().getTime() <= endDate.getTime()
+      dayjs(event.startTime).isSameOrAfter(dayjs(startDate)) &&
+      dayjs(event.startTime).isBefore(dayjs(endDate).add(1, "day"))
   );
 
   return filteredEvents;
 }
 
-function convertTZ(date: Date, tzString: string) {
-  return new Date(
-    date.toLocaleString("en-US", {
-      timeZone: tzString,
-    })
+function filterEventsbyDate(
+  events: FirebaseFirestore.DocumentData[],
+  userDate: string
+) {
+  const filteredEvents = events.filter(
+    (event: FirebaseFirestore.DocumentData) => event.date === userDate
   );
+  return filteredEvents;
 }
